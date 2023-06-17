9cdcb0c8acd524343ed261e430bfdf1468c44eca
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2023-06-17 13:26:21 +0100

    init

---
+++ src/app/about/page.js
@@ -0,0 +1,3 @@
+export default () => {
+    return (<div>About this website</div>)
+}
---
+++ src/app/api/book/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+const winston = require('winston');
+
+const papertrail = new winston.transports.Http({
+  host: 'logs.collector.solarwinds.com',
+  path: '/v1/log',
+  auth: { username: new String(''), password: '5TgyqPpzUhYKbGt7xCI_LC7omCTf' },
+  ssl: true,
+});
+
+const logger = winston.createLogger({
+  transports: [papertrail],
+});
+
+
+export const GET = (req) => {
+    logger.info({msg: "hello", price: 100});
+
+    return NextResponse.json("Hello World");
+}
